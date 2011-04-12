a421538f50fc6a3e8f603969f598f9620ecc766b
Ju Liu <ju.liu@welaika.com>
2011-04-12 15:18:20 +0200

    added rails3 support

--- lib/shutup_sqlite.rb
+++ lib/shutup_sqlite.rb
@@ -1,6 +1,20 @@
 class ActiveRecord::ConnectionAdapters::SQLiteAdapter
-  # Aaaaargh
-  def log_info(sql, name, ms)
-    super(sql, name, ms) unless sql.match(/^\s*SELECT\s+name\s+FROM\s+sqlite_master\s+/)
+
+  def shutup?(sql)
+    sql.match(/^\s*SELECT\s+name\s+FROM\s+sqlite_master\s+/)
   end
+
+  def log_info(sql, name, ms)
+    super(sql, name, ms) unless shutup?(sql)
+  end
+
+  def execute(sql, name = nil)
+    unless shutup?(sql)
+      log(sql, name) { @connection.execute(sql) }
+    else
+      @connection.execute(sql)
+    end
+  end
+
 end
+
